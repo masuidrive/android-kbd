fd6ccaa228c454470dc7acff6fe1411a4c3385e4
Yuichiro MASUI <masui@masuidrive.jp>
2026-09-11 11:50:38 +0900

    [260911-022705-rebuild-keyboard-visual-fidelity] build: set version 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.masuidrive.gestureime"
         minSdk = 28
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk { abiFilters += "arm64-v8a" }
     }
