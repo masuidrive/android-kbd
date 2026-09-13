cd0dcedc146a4a54d4b948cd9368809614067934
Yuichiro MASUI <masui@masuidrive.jp>
2026-09-13 12:31:42 +0900

    [260911-063912-fix-intermittent-keyboard-vertical-offset] chore(release): prepare v0.15.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.masuidrive.gestureime"
         minSdk = 28
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.15.3"
+        versionCode = 20
+        versionName = "0.15.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk { abiFilters += "arm64-v8a" }
     }
